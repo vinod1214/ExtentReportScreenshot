114ae65fbc0414e936de41603cc94687a60727b8
vinod1214 <kumar.vinod1214@yahoo.in>
2020-07-27 17:53:16 +0530

    first commit

---
+++ src/test/java/FreeCRMTest.java
@@ -0,0 +1,104 @@
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class FreeCRMTest {
+
+	public WebDriver driver;
+	public ExtentReports extent;
+	public ExtentTest extentTest;
+	
+	@BeforeTest
+	public void setExtent() {
+		extent = new ExtentReports(System.getProperty("user.dir")+"/test-output/ExtentReport.html",true);
+		extent.addSystemInfo("Host Name", "Vinod Windows");
+		extent.addSystemInfo("User Name", "Vinod Duddukuri");
+		extent.addSystemInfo("Environmemt", "QA");
+	}
+	
+	@AfterTest
+	public void endReport() {
+		extent.flush();
+		extent.close();
+	}
+	
+	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException{
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/" + screenshotName + dateName
+				+ ".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(source,finalDestination);
+		return destination;
+	}
+	
+	@BeforeMethod
+	public void setUp() {
+		//System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\drivers\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://classic.crmpro.com/index.html");
+	}
+	
+	@Test
+	public void freeCrmTitleTest() {
+		extentTest = extent.startTest("freeCrmTitleTest");
+		String title = driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.123");
+	}
+	
+	@Test
+	public void freemCRMLogoTest(){
+		extentTest = extent.startTest("freemCRMLogoTest");
+		boolean b = driver.findElement(By.xpath("//img[@class='img-responsive111']")).isDisplayed();
+		Assert.assertTrue(b);
+	}
+	
+	@AfterMethod
+	public void tearDown(ITestResult result) throws IOException {
+		if(result.getStatus()==ITestResult.FAILURE){
+			extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS "+result.getName());
+			extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS "+result.getThrowable());
+			String screenshotPath = FreeCRMTest.getScreenshot(driver, result.getName());
+			extentTest.log(LogStatus.FAIL, extentTest.addScreenCapture(screenshotPath));
+		}
+		else if(result.getStatus()==ITestResult.SKIP){
+			extentTest.log(LogStatus.SKIP, "Test Case SKIPPED IS " + result.getName());
+		}
+		else if(result.getStatus()==ITestResult.SUCCESS){
+			extentTest.log(LogStatus.PASS, "Test Case PASSED IS " + result.getName());
+
+		}
+		extent.endTest(extentTest);
+		driver.quit();
+	}
+}
